72ccd789d6e6328647833ea2cb311f4dcf2835c2
Jesse <JuicinessJMN@Gmail.com>
2023-03-12 18:31:26 -0500

    need to find how to start

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const xyz = require('../models');
+// Don't know if needed
+// const withAuth = require('../utils/auth');
+
+// Get All
+// router.get('/', async (req, res) => {
+//     try {
+
+//     }
+// })
+
+// Get One
+// router.get('/something/:id', async (req, res) => {
+//     try {
+
+//     }
+// })
+
+
+
+
+
+
+
+module.exports = router;
